75ef27d037c0369753d4d64bbbc75ab062aab50e
AbreuY <yolfran12@gmail.com>
2021-10-30 13:54:30 -0500

    functional exercise. Needs to add design

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-
+import "../styles/custom.css";
 //import your own components
 import Home from "./component/home.jsx";
 
